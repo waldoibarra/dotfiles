286a8e1f70013093d326e8a80a14fa043cf754f2
Waldo Ibarra <waldo.ibarra@linux.com>
2026-09-21 21:18:10 -0600

    chore(terminals): increase font size to 16

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -25,7 +25,7 @@ config.default_prog = { wezterm.home_dir .. '/.local/share/mise/shims/herdr' }
 -- │                                   FONT                                       │
 -- └──────────────────────────────────────────────────────────────────────────────┘
 
-config.font_size = 14
+config.font_size = 16
 
 -- ┌──────────────────────────────────────────────────────────────────────────────┐
 -- │                                  WINDOW                                      │
